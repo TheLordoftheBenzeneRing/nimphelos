dfc2156c6160bacb47cb137ddaee31f48c7a411a
TheLordoftheBenzeneRing <jtc3wv@virginia.edu>
2023-10-24 16:24:23 -0400

    Updated

--- calculate.py
+++ calculate.py
@@ -1,48 +1,31 @@
-import torch, matplotlib.pyplot as plt, numpy as np
+import torch,matplotlib.pyplot as plt, numpy as np
 from torch import Tensor
 
 k_J = 1.380649e-23
-k_G = 20.83661912
+k_M = 20.83661912e3
 c = 2.99792458e8
 e = 8.8541878128e-12
 h = 6.62607015e-34
 
-class molecule():
-    def __init__(
-            self,
-            rot: tuple,  # Rotational constants as a tuple of A,B,C in units of GHz
-            dip: tuple,  # Dipole moments along the principle axes in order of A,B,C in units of debye
-            mass: float, # Mass of the molecule in amu
-            type: int=0  # An integer to designate the type of molecule: 0 -> Diatomic, 1 -> Linear, 2 -> Spherical, 3 -> Symmetric, 4 -> Asymmetric
-            ) -> None:
-        self.rot,self.dip,self.type = rot,dip,type
-        self.mass = 1.66054e-27*mass
-    
-    def energy(self) -> Tensor:
+def linear(temperature,B):
 
-        if self.type == 0:
-            J = torch.arange(0,300)
-            return self.rot[1]*J*(J+1)
-    
-    def get_lines(self,T: float) -> Tensor:
-        vs,fs = [],[]
+    J = torch.arange(0,100)
+    energy = B*J*(J+1)
+    frequency = torch.diff(energy)
+    FWHM = 2*frequency*np.sqrt((2*k_J*temperature*np.log(2))/(1e-27*c**2))
 
-        if self.type == 0:
-            J = torch.arange(0,300)
-            E = self.energy()
-            freq = torch.diff(E)
-            P = (2*J + 1) * torch.exp(-E/(k_G*T))
-            Q = torch.sum(P)
-            frac = P/Q
-            FWHM = 2*freq*np.sqrt(2*k_J*T*np.log(2)/(self.mass*c**2))
-            constant = ((self.dip[1]*3.33564e-30)**2 * 16 * np.pi**2) * np.sqrt((self.mass * c**2) / (2 * np.pi * k_J * T)) / (3 * e * h * c**3)
-            for (fr,F,fra) in zip(freq[frac[1:]>1e-5],FWHM[frac[1:]>1e-5],frac[1:][frac[1:]>1e-5]):
-                v = torch.linspace(fr-3*F,fr+3*F,100)
-                f = fra*constant*fr**2*torch.exp(-(self.mass*c**2*(v - fr)**2)/(2*k_J*T*fr**2))
-                vs += v.tolist()
-                fs += f.tolist()
-            plt.plot(vs,fs)
-            plt.show()
+    weight = (2*J + 1) * torch.exp(-energy/(k_M*temperature))
+    Q = torch.sum(weight)
+    fraction = weight/Q
 
-CO = molecule((0,57.897869,0),(0,0.122,0),28.01)
-CO.get_lines(100)
+    nus,lines = [],[]
+    constant = np.sqrt((1e-27*c**2) / (2*np.pi*k_J*temperature)) * ((16*np.pi**3*(3.33564e-30)**2) / (3*e*h*c**3))
+    for (freq,FW,frac) in zip(frequency[fraction[1:]<1e-5],FWHM[fraction[1:]<1e-5],fraction[1:][fraction[1:]>1e-5]):
+        nu = torch.linspace(freq-3*FW,freq+3*FW,100)
+        line = constant * frac * freq**2 * torch.exp(-(1e-27 * c**2 * (nu-freq)**2) / (2*k_J*temperature*freq**2))
+        nus += nu.tolist()
+        lines += line.tolist()
+    plt.plot(nus,np.array(lines)/max(lines))
+    plt.show()
+
+linear(100,57000)
