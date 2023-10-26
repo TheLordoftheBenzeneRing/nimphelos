590e73a5ab12355478d731a1fc57a6a58962d6f3
TheLordoftheBenzeneRing <jtc3wv@virginia.edu>
2023-10-26 17:43:44 -0400

    Working on display

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import numpy as np,matplotlib.pyplot as plt
 
+def create_table():
+    table_element = Element("displayables")
+    table_element.select(".display:none").remove_class("display:none")
+
 k_J = 1.380649e-23
 k_M = 20.83661912e3
 c = 2.99792458e8
@@ -14,17 +18,17 @@ def diatomic():
     frequency = np.diff(energy)
     FWHM = 2*frequency*np.sqrt((2*k_J*temperature*np.log(2))/(1e-27*c**2))
 
-    weight = (2*J + 1) * np.exp(-energy/(k_M*temperature))
-    Q = np.sum(weight)
-    fraction = weight/Q
+    # weight = (2*J + 1) * np.exp(-energy/(k_M*temperature))
+    # Q = np.sum(weight)
+    # fraction = weight/Q
 
-    nus,lines = [],[]
-    mask = fraction[1:]>1e-7
-    constant = np.sqrt((1e-27*c**2) / (2*np.pi*k_J*temperature)) * ((16*np.pi**3*(3.33564e-30)**2) / (3*e*h*c**3))
-    for (freq,FW,frac) in zip(frequency[mask],FWHM[mask],fraction[1:][mask]):
-        nu = np.linspace(freq-3*FW,freq+3*FW,100)
-        line = constant * frac * freq**2 * np.exp(-(1e-27 * c**2 * (nu-freq)**2) / (2*k_J*temperature*freq**2))
-        nus += nu.tolist()
-        lines += line.tolist()
-    plt.plot(nus,np.array(lines)/max(lines))
-    display(plt,target="output")
+    # nus,lines = [],[]
+    # mask = fraction[1:]>1e-7
+    # constant = np.sqrt((1e-27*c**2) / (2*np.pi*k_J*temperature)) * ((16*np.pi**3*(3.33564e-30)**2) / (3*e*h*c**3))
+    # for (freq,FW,frac) in zip(frequency[mask],FWHM[mask],fraction[1:][mask]):
+    #     nu = np.linspace(freq-3*FW,freq+3*FW,100)
+    #     line = constant * frac * freq**2 * np.exp(-(1e-27 * c**2 * (nu-freq)**2) / (2*k_J*temperature*freq**2))
+    #     nus += nu.tolist()
+    #     lines += line.tolist()
+    # plt.plot(nus,np.array(lines)/max(lines))
+    # display(plt,target="output")
